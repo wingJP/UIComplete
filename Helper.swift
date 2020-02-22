38296c5eaca2b83ece2a1c1af790e34369663ee8
waing <waingjianpeng@outlook.com>
2020-02-22 16:53:56 +0800

    完成 class装换

--- Helper.swift
+++ Helper.swift
@@ -13,16 +13,13 @@ class Helper: NSObject {
 }
 
 
-func jsonToArray(path: String) -> Array<[String: String]>? {
+func jsonToArray(path: String) -> Array<[String: Any]>? {
     let url = URL(fileURLWithPath: path)
     guard let jsonData = try? Data(contentsOf: url, options: .alwaysMapped) else {
         return nil
     }
-    guard let jsonResult = try? JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.mutableContainers) as? Array<[String: String]> else {
+    guard let jsonResult = try? JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.mutableContainers) as? Array<[String: Any]> else {
         return nil
     }
-//    guard let result = jsonResult as? [[String:String]] else {
-//        return nil
-//    }
     return jsonResult
 }
--- UIComplete/ViewController.swift
+++ UIComplete/ViewController.swift
@@ -12,15 +12,25 @@ let jsonPath = Bundle.main.path(forResource: "model", ofType: "json")
 
 class ViewController: UITableViewController {
     
-    var cellArray = [[String:Any?]]()
+    var cellArray = [[String: Any?]]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let path = jsonPath ,
-           let arr = jsonToArray(path: path) {
+        
+        if cellArray.count == 0,
+            let path = jsonPath,
+            let arr = jsonToArray(path: path) {
             cellArray = arr
             self.tableView.reloadData()
         }
-        // Do any additional setup after loading the view.
+    }
+
+     static func new(array: [[String: Any?]]) -> ViewController? {
+        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+        if let ac = storyBoard.instantiateViewController(withIdentifier: "ViewController") as?  ViewController {
+            ac.cellArray = array
+            return ac
+        }
+        return nil
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -32,13 +42,48 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         if let title = cellArray[indexPath.row]["title"] as? String{
             cell.textLabel?.text = title
         }
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        guard let title = cellArray[indexPath.row]["title"] as? String else {
+            return
+        }
+        if let className = cellArray[indexPath.row]["class"] as? String ,
+            let vc = getViewController(name: className){
+            vc.title = title
+            self.navigationController?.pushViewController(vc, animated: true)
+        }else if let arr = cellArray[indexPath.row]["sub"] as? [[String : Any?]],
+            let vc = ViewController.new(array: arr ){
+            vc.title = title
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    func getViewController(name : String) -> UIViewController? {
         
+        guard let type: UIViewController.Type = self.classFromString(name: name) as? UIViewController.Type else {
+            let error = "class \(name) have not fund"
+            let alest = UIAlertController(title: "error", message: error, preferredStyle: .alert)
+            alest.addAction(UIAlertAction.init(title: "OK", style: .default, handler:nil))
+            self.present(alest, animated: true, completion: nil)
+            return nil
+        }
+        
+        let vc : UIViewController = type.init()
+        return vc
+    }
+    
+    func classFromString(name: String) -> AnyClass? {
+        let bundleIdentifier = (Bundle.main.infoDictionary!["CFBundleIdentifier"] as! String).components(separatedBy: ".").last!
+        let className = bundleIdentifier + "." + name
+        let convertedName = String(className.map { $0 == "-" ? "_" : $0 })
+        return NSClassFromString(convertedName)
     }
 }
 
