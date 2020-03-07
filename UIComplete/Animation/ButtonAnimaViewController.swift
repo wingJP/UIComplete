cf9ee197b37adc00d03d2ad1d99faf6844afaa50
waing <waingjianpeng@outlook.com>
2020-03-07 22:31:47 +0800

    完成按钮弹出动画

--- UIComplete/Animation/ButtonAnimaViewController.swift
+++ UIComplete/Animation/ButtonAnimaViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
-
+import SnapKit
 class ButtonAnimaViewController: UIViewController {
-
+    
+    lazy var addButton : UIButton = {
+        var btn = UIButton(type: .system)
+        btn.setTitle("show", for: .normal)
+        btn.addTarget(self, action: #selector(showAction), for: .touchUpInside)
+        return btn
+    }()
+    
+    lazy var closeButton : UIButton = {
+          var btn = UIButton(type: .system)
+          btn.setTitle("close", for: .normal)
+          btn.addTarget(self, action: #selector(dismissAction), for: .touchUpInside)
+          return btn
+      }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
         initSubView()
+        
+        self.view.addSubview(addButton)
+        addButton.snp.makeConstraints { (make) in
+            make.centerY.equalTo(self.view)
+            make.left.equalTo(self.view).offset(100)
+        }
+        self.view.addSubview(closeButton)
+        closeButton.snp.makeConstraints { (make) in
+            make.centerY.equalTo(self.view)
+            make.left.equalTo(self.view).offset(200)
+        }
+        
         // Do any additional setup after loading the view.
     }
     
@@ -26,20 +53,64 @@ class ButtonAnimaViewController: UIViewController {
         scrollView.showsVerticalScrollIndicator = false
         scrollView.clipsToBounds = false
         scrollView.snp.makeConstraints { (make) in
-            make.edges.equalTo(self)
+            make.left.right.equalTo(self.view)
+            make.bottom.equalTo(self.view.safeAreaInsets)
+            make.height.equalTo(120)
         }
-        
         reloadData()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    var buttonArray : [UIButton] = []
+    let space = 20
+    func reloadData() {
+        var lastBtn : UIButton?
+        for _ in 0..<6 {
+            let button = UIButton(type: .custom)
+            self.scrollView.addSubview(button)
+            button.layer.cornerRadius = 30
+            button.backgroundColor = UIColor.randomColor
+            button.snp.makeConstraints { (make) in
+                make.height.width.equalTo(60)
+                make.centerY.equalTo(self.scrollView)
+                make.left.equalTo(lastBtn?.snp.right ?? self.scrollView.snp.left).offset(space)
+            }
+            buttonArray.append(button)
+            lastBtn = button
+        }
+        lastBtn!.snp.makeConstraints { (make) in
+            make.right.equalTo(self.scrollView).offset(-space)
+        }
+        buttonTransform()
     }
-    */
-
+    
+    func buttonTransform() {
+        for button in self.buttonArray {
+            button.transform = CGAffineTransform.init(translationX: 0, y: 100)
+        }
+    }
+    
+    @objc func showAction(){
+        for (index, button) in self.buttonArray.enumerated() {
+            UIView.animate(withDuration: 0.75, delay: Double(index) * 0.05,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .layoutSubviews,
+                           animations: {
+                            button.transform = CGAffineTransform.identity
+            }, completion: nil)
+        }
+    }
+    
+    @objc func dismissAction(){
+        for (index, button) in self.buttonArray.enumerated() {
+            UIView.animate(withDuration: 0.75, delay: Double(index) * 0.05,
+                           usingSpringWithDamping: 0.5,
+                           initialSpringVelocity: 0.5,
+                           options: .layoutSubviews,
+                           animations: {
+                            button.transform = CGAffineTransform.init(translationX: 0, y: 100)
+            }, completion: nil)
+        }
+    }
 }
